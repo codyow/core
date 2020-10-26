d35bc46de9bf1fd51b51c8ea9365b0637f3b5d84
Cody <cody@getopenwater.com>
2020-10-26 16:37:02 -0500

    Adding main css files.

--- js/v1/core.js
+++ js/v1/core.js
@@ -1,24 +1,7 @@
-// Chat Pop Up
-var chatset = $('#chatset').css('padding');
+// Chat Popup
+$( ".nav-menu > ul > li:last-child a" ).replaceWith( $( ".chat-pop" ) );
 
-if (chatset == '1'){
-    $( ".nav-menu ul li:nth-child(1) a").replaceWith( $( ".chat-pop" ));
-}elseif(chatset == '2'){
-    $( ".nav-menu ul li:nth-child(2) a").replaceWith( $( ".chat-pop" ));
-}elseif(chatset == '3'){
-    $( ".nav-menu ul li:nth-child(3) a").replaceWith( $( ".chat-pop" ));
-}elseif(chatset == '4'){
-    $( ".nav-menu ul li:nth-child(4) a").replaceWith( $( ".chat-pop" ));
-}elseif(chatset == '5'){
-    $( ".nav-menu ul li:nth-child(5) a").replaceWith( $( ".chat-pop" ));
-}elseif(chatset == '6'){
-    $( ".nav-menu ul li:nth-child(6) a").replaceWith( $( ".chat-pop" ));
-}elseif(chatset == '7'){
-    $( ".nav-menu ul li:nth-child(7) a").replaceWith( $( ".chat-pop" ));
-}elseif(chatset == '8'){
-    $( ".nav-menu ul li:nth-child(8) a").replaceWith( $( ".chat-pop" ));
-}elseif(chatset == '9'){
-    $( ".nav-menu ul li:nth-child(9) a").replaceWith( $( ".chat-pop" ));
-}elseif(chatset == '10'){
-    $( ".nav-menu ul li:nth-child(10) a").replaceWith( $( ".chat-pop" ));
+// Replace My Account with Login When Logged Out
+if($('.nav-menu').text().indexOf("Log Out") === -1) {
+    $("a:contains('My Account')").replaceWith('<a href="https://find-a-template.secure-platform.com/a/account/login">Login</a>');
 }
