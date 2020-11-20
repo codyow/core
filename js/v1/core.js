832057c8da5ddeb51ca22ef85f0230a38e527e4b
Cody <cody@getopenwater.com>
2020-11-20 12:17:17 -0600

    Core JS Merge
    - Added Asc/Desc
    - Added Wait Til Exist.

--- js/v1/core.js
+++ js/v1/core.js
@@ -5,3 +5,37 @@ $( ".nav-menu > ul > li:last-child a" ).replaceWith( $( ".chat-pop" ) );
 if($('.nav-menu').text().indexOf("Log Out") === -1) {
     $("a:contains('My Account')").replaceWith('<a href="https://find-a-template.secure-platform.com/a/account/login">Login</a>');
 }
+
+// Asc/Desc
+$(window).load(function () {
+    var $gallerySort = $('.results-sort option');
+    if ($gallerySort.length > 0) {
+        // it exists so now alter the text
+        $gallerySort.each((key, value) => {
+            var $value = $(value); // cache your jQuery objects, it makes life easier, faster and cleaner
+            $value.text(
+                $value.text().replace('ASC', 'A-Z').replace('DESC', 'Z-A')
+            );
+        });
+    }
+});
+
+// Wait Til Exist
+$.fn.waitUntilExists = function(handler, shouldRunHandlerOnce, isChild) {
+	  var found = 'found';
+	  var $this = $(this.selector);
+	  var $elements = $this.not(function() {
+		  return $(this).data(found);
+	  }).each(handler).data(found, true);
+
+	  if (!isChild) {
+		  (window.waitUntilExists_Intervals = window.waitUntilExists_Intervals || {})[this.selector] =
+		  window.setInterval(function() {
+			  $this.waitUntilExists(handler, shouldRunHandlerOnce, true);
+		  }, 500);
+	  } else if (shouldRunHandlerOnce && $elements.length) {
+		  window.clearInterval(window.waitUntilExists_Intervals[this.selector]);
+	  }
+
+	  return $this;
+  }
