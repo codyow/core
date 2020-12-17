97a1035ac375d7326ee67479beac93228c7ddf22
Cody <cody@getopenwater.com>
2020-12-17 09:28:34 -0600

    Backups
    - Updated VC and Poster
    - Core JS / Fixed Login/My Account linking issue.

--- js/v1/core.js
+++ js/v1/core.js
@@ -3,7 +3,7 @@ $( ".nav-menu > ul > li:last-child a" ).replaceWith( $( ".chat-pop" ) );
 
 // Replace My Account with Login When Logged Out
 if($('.nav-menu').text().indexOf("Log Out") === -1) {
-    $("a:contains('My Account')").replaceWith('<a href="https://find-a-template.secure-platform.com/a/account/login">Login</a>');
+    $("a:contains('My Account')").replaceWith('<a href="/a/account/login">Login</a>');
 }
 
 // Asc/Desc
