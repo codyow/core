4508ab1fd69d66fbd23d48e4fde4cdd6c3770519
Cody <cody@getopenwater.com>
2021-03-25 17:52:36 -0500

    Adding Data-Compare function to conditioning.js.

--- js/v1/conditioning.js
+++ js/v1/conditioning.js
@@ -15,3 +15,11 @@ $('.data-message').waitUntilExists(function (){
         $(this).show();
     }
 });
+
+$('.data-compare').waitUntilExists(function (){
+	var dataCheck = $(this).data("test");
+	var dataCompare = $(this).data("compare");
+	if(dataCheck != dataCompare){
+		$(this).remove();
+	}
+});
